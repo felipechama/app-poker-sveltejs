6b6785c25813240376ab4d5f2efbef4c1362dc1e
Felipe Chama <felipechama42@gmail.com>
2019-06-03 23:32:11 -0300

    Create Players page

---
+++ src/components/Player.svelte
@@ -0,0 +1,45 @@
+<script>
+  export let name;
+  export let slug;
+
+  const onErrorImage = (e) => {
+    e.target.src = '../images/players/default.jpg';
+  }
+</script>
+
+<style>
+  .container {
+    display: flex;
+    margin: 8px 0 0 0;
+    padding: 8px 0 0 0;
+    align-items: center;
+    flex-direction: row;
+    border-top: 1px solid #eee;
+  }
+
+  .image {
+    width: 50px;
+    height: 50px;
+    overflow: hidden;
+    border-radius: 50%;
+    border: 1px solid #ddd;
+  }
+
+  .image img {
+    width: 100%;
+    height: auto;
+  }
+
+  .name {
+    font-size: 14px;
+    margin-left: 10px;
+  }
+</style>
+
+<div class="container">
+  <div class="image">
+    <img src="../images/players/{slug}.jpg" alt="{name}" on:error={onErrorImage}>
+  </div>
+
+  <p class="name">{name}</p>
+</div>
--- src/pages/Players.svelte
+++ src/pages/Players.svelte
@@ -1,17 +1,44 @@
 <script>
-  import Loading from '../components/Loading.svelte';
+  import { onMount } from 'svelte';
 
-  let loading = false;
+  import Loading from '../components/Loading.svelte';
+  import Player from '../components/Player.svelte';
+  import { fetchApi } from '../utils';
+
+  let loading = true;
+  let players = [];
+
+  onMount(async () => {
+    const res = await fetchApi('/players');
+    players = res;
+    loading = false;
+  });
 </script>
 
 <style>
-
+  .title {
+    font-weight: bold;
+    margin-bottom: 15px;
+  }
 </style>
 
 {#if loading}
   <Loading />
 {:else}
   <div>
-    players page
+    <h2 class="title">
+      Jogadores
+    </h2>
+
+    <ul class="list-players">
+      {#each players as player}
+        <li>
+          <Player
+            name={player.name}
+            slug={player.slug}
+          />
+        </li>
+      {/each}
+    </ul>
   </div>
 {/if}
